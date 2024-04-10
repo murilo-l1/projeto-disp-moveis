74e377a66894bb141bcb9cbf59646309ea469867
murilo-l1 <murilolusvarghi@alunos.utfpr.edu.br>
2024-04-09 23:03:58 -0300

    first commit basic config

---
+++ expense_tracker/lib/pages/home.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar()
+    );
+  }
+}
+
+AppBar appBar() {
+    return AppBar(
+      //definicao do titulo e estilizacao dele
+      title: const Text( 
+        'Expense Tracker',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      // definindo um detector de acoes ocorridas dentro da AppBar com suporte para adicao de metodos em cada interacao com um componente
+      leading: GestureDetector(
+        onTap: () {
+          
+        },
+        child: Container(
+          margin: const EdgeInsets.all(18),
+          alignment: Alignment.center,
+          // usando um asset que importamos para ser o icone dessa parte da appBar
+           child: SvgPicture.asset( 
+            'assets/icons/hamburger.svg',
+            height: 25,
+            width: 25,
+          ), 
+          decoration: BoxDecoration(
+            color: const Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10)
+          ),
+        ),
+      ),
+      // aqui as acoes sao definidas em formato de lista
+      actions: [
+        GestureDetector(
+          onTap: () {},
+          child: Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+             child: SvgPicture.asset(
+              'assets/icons/profile.svg',
+              height: 20,
+              width: 20,
+            ),  
+          ),
+        ),
+      ],
+    );
+  }
