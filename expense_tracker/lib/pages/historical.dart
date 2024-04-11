cd7e7d7d412741b3f5d8b1ba9a8e44892bc6dac4
murilo-l1 <murilolusvarghi@alunos.utfpr.edu.br>
2024-04-10 23:03:30 -0300

    Improved HomePage

---
+++ expense_tracker/lib/pages/historical.dart
@@ -0,0 +1,15 @@
+import 'package:expense_tracker/pages/home.dart';
+import 'package:flutter/material.dart';
+
+class HistoricalPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(context),
+      body: Center(
+        child: Text('This is another screen.'),
+      ),
+      bottomNavigationBar: buildBottomNavigationBar(context),
+    );
+  }
+}
--- expense_tracker/lib/pages/home.dart
+++ expense_tracker/lib/pages/home.dart
@@ -1,15 +1,18 @@
 import 'dart:js';
+import 'package:expense_tracker/pages/historical.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(context),
-      body: Center(
+      appBar: buildAppBar(context),
+      // alterar Padding para ajustar a distancia depois que eu colocar o grafico!
+      body: Padding(
+        padding: const EdgeInsets.only(top: 500, bottom: 20), // Ajuste a quantidade conforme necessário
         child: ListView(
           shrinkWrap: true,
           children: [
@@ -20,11 +23,12 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
+      bottomNavigationBar: buildBottomNavigationBar(context),
     );
   }
 }
 
-AppBar appBar(BuildContext context) {
+AppBar buildAppBar(BuildContext context) {
   return AppBar(
     //definicao do titulo e estilizacao dele
     title: const Text(
@@ -38,14 +42,14 @@ AppBar appBar(BuildContext context) {
     // definindo um detector de acoes ocorridas dentro da AppBar com suporte para adicao de metodos em cada interacao com um componente
     leading: GestureDetector(
       onTap: () {
-        Scaffold.of(context).openEndDrawer();
+        // switch pro dark mode
       },
       child: Container(
         margin: const EdgeInsets.all(18),
         alignment: Alignment.center,
         // usando um asset que importamos para ser o icone dessa parte da appBar
         child: SvgPicture.asset(
-          'assets/icons/hamburger.svg',
+          'assets/icons/dark-theme.svg',
           height: 25,
           width: 25,
         ),
@@ -148,8 +152,9 @@ void openExpenseBox(BuildContext context) {
 
 class ExpenseItem extends StatelessWidget {
   final String category;
+  double itemValue = 0.0;
 
-  const ExpenseItem({required this.category});
+  ExpenseItem({required this.category});
 
   @override
   Widget build(BuildContext context) {
@@ -161,16 +166,73 @@ class ExpenseItem extends StatelessWidget {
           border: Border(bottom: BorderSide(color: Colors.grey[300]!)),
         ),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
-              category,
+              'R\$' + itemValue.toString(),
               style: TextStyle(fontSize: 18.0),
             ),
-            Icon(Icons.arrow_forward_ios),
+            Spacer(),
+            Row(
+              children: [
+                getCategoryIcon(category),
+                SizedBox(width: 16.0),
+                Text(
+                  category,
+                  style: TextStyle(fontSize: 18.0),
+                ),
+              ],
+            ),
+            Spacer(),
+            Icon(Icons.add),
           ],
         ),
       ),
     );
   }
+}
+
+
+Icon getCategoryIcon(String category) {
+  switch (category) {
+    case 'Lazer':
+      return Icon(Icons.star); // Change the icon as per your preference
+    case 'Saúde':
+      return Icon(Icons.local_hospital);
+    case 'Transporte':
+      return Icon(Icons.directions_car);
+    case 'Alimentação':
+      return Icon(Icons.restaurant_menu);
+    default:
+      return Icon(Icons.category);
+  }
+}
+
+// esse método cria a barra de navegação inferior, usado para navegar entre telas (Home e Histórico)
+BottomNavigationBar buildBottomNavigationBar(BuildContext context) {
+  return BottomNavigationBar(
+    backgroundColor: Color.fromARGB(255, 213, 203, 155),
+    items: const <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(Icons.pie_chart_sharp),
+        label: 'Home',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.history),
+        label: 'Gastos Anteriores',
+      ),
+    ],
+    onTap: (int index) {
+      if (index == 0) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomePage()),
+        );
+      } else if (index == 1) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HistoricalPage()),
+        );
+      }
+    },
+  );
 }
