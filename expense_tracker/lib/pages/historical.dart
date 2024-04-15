f2448ffe07b6e3751d88c1691bb620c78c427fdc
murilo-l1 <murilolusvarghi@alunos.utfpr.edu.br>
2024-04-15 18:13:13 -0300

    refactoring the charts

--- expense_tracker/lib/pages/historical.dart
+++ expense_tracker/lib/pages/historical.dart
@@ -1,10 +1,9 @@
-import 'dart:math';
 
 import 'package:expense_tracker/models/enums.dart';
 import 'package:expense_tracker/pages/home.dart';
 import 'package:expense_tracker/pages/profile/components/customBottomBar.dart';
 import 'package:flutter/material.dart';
-import 'package:fl_chart/fl_chart.dart';
+import 'package:expense_tracker/charts/barChart.dart';
 
 class HistoricalPage extends StatefulWidget {
   const HistoricalPage({super.key});
@@ -65,7 +64,7 @@ class _HistoricalPageState extends State<HistoricalPage> {
             padding: const EdgeInsets.only(top: 20), // Ajuste conforme necessário
             child: SizedBox(
               height: 200,
-              child: _BarChart(monthExpenses: monthExpenses),
+              child: BarChartImpl(monthExpenses: monthExpenses),
               
             ),
           ),
@@ -79,186 +78,6 @@ class _HistoricalPageState extends State<HistoricalPage> {
       ),
     );
   }
-}
-
-//criar um grafico de barras com 12 barras, uma para cada mes do ano
-class IndividualBar {
-  final int x; 
-  final double y;
- 
-  IndividualBar({
-    required this.x, 
-    required this.y
-    });
-}
-
-class BarData{
-  final double januaryAmount;
-  final double februaryAmout;
-  final double marchAmout;
-  final double aprilAmount;
-  final double mayAmount;
-  final double juneAmount;
-  final double julyAmount;
-  final double augustAmount;
-  final double septemberAmount;
-  final double octoberAmount;
-  final double novemberAmount;
-  final double decemberAmount;
-
-  BarData({
-    required this.januaryAmount,
-    required this.februaryAmout,
-    required this.marchAmout,
-    required this.aprilAmount,
-    required this.mayAmount,
-    required this.juneAmount,
-    required this.julyAmount,
-    required this.augustAmount,
-    required this.septemberAmount,
-    required this.octoberAmount,
-    required this.novemberAmount,
-    required this.decemberAmount
-  });
-
-  List<IndividualBar> barData = [];
-
-
-  //inicializando os valores das barras respectivos aos gastos de cada mes
-  void initializeBarData(){
-      barData = [
-        IndividualBar(x: 0, y: januaryAmount),
-        IndividualBar(x: 1, y: februaryAmout),
-        IndividualBar(x: 2, y: marchAmout),
-        IndividualBar(x: 3, y: aprilAmount),
-        IndividualBar(x: 4, y: mayAmount),
-        IndividualBar(x: 5, y: juneAmount),
-        IndividualBar(x: 6, y: julyAmount),
-        IndividualBar(x: 7, y: augustAmount),
-        IndividualBar(x: 8, y: septemberAmount),
-        IndividualBar(x: 9, y: octoberAmount),
-        IndividualBar(x: 10, y: novemberAmount),
-        IndividualBar(x: 11, y: decemberAmount),
-        ];
-
-  }
-}
-
-
-class _BarChart extends StatelessWidget {
-  final List monthExpenses;
-  const _BarChart({super.key, required this.monthExpenses});
-
-  @override
-  Widget build(BuildContext context) {
-    BarData barData = BarData(
-      januaryAmount: monthExpenses[0],
-      februaryAmout: monthExpenses[1],
-      marchAmout: monthExpenses[2],
-      aprilAmount: monthExpenses[3],
-      mayAmount: monthExpenses[4],
-      juneAmount: monthExpenses[5],
-      julyAmount: monthExpenses[6],
-      augustAmount: monthExpenses[7],
-      septemberAmount: monthExpenses[8],
-      octoberAmount: monthExpenses[9],
-      novemberAmount: monthExpenses[10],
-      decemberAmount: monthExpenses[11],
-    );
-    barData.initializeBarData();
-
-    return SizedBox(
-      height: 250, 
-      child: BarChart(
-        BarChartData(
-          maxY: 100,
-          minY: 0,
-          gridData: const FlGridData(show: false),
-          borderData: FlBorderData(show: false),
-          barTouchData: BarTouchData(enabled: false),
-          titlesData: const FlTitlesData(
-            show: true,
-            topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-            leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-            rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-            bottomTitles: AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: true,
-                getTitlesWidget: getBottomTitles,
-              ),
-            ),
-          ),
-          barGroups: barData.barData.map(
-            (data) => BarChartGroupData(
-              x: data.x,
-              barRods: [
-                BarChartRodData(
-                  toY: data.y,
-                  color: Colors.blue[500],
-                  width: 20,
-                  borderRadius: BorderRadius.circular(6),
-                )
-              ],
-            ),
-          ).toList(),
-        ),
-      ),
-    );
-  }
-}
-
-Widget getBottomTitles(double value, TitleMeta meta){
-
-  const style = TextStyle(
-    fontWeight: FontWeight.bold,
-    fontSize: 10,
-  );
-
-  Widget text;
-
-  switch(value.toInt()){
-    case 0:
-      text = const Text('Jan', style: style);
-      break;
-    case 1:
-      text = const Text('Fev', style: style);
-      break;
-    case 2:
-      text = const Text('Mar', style: style);
-      break;
-    case 3:
-      text = const Text('Abr', style: style);
-      break;
-    case 4:
-      text = const Text('Mai', style: style);
-      break;
-    case 5:
-      text = const Text('Jun', style: style);
-      break;
-    case 6:
-      text = const Text('Jul', style: style);
-      break;
-    case 7:
-      text = const Text('Ago', style: style);
-      break;
-    case 8:
-      text = const Text('Set', style: style);
-      break;
-    case 9: 
-      text = const Text('Out', style: style);
-      break;
-    case 10:
-      text = const Text('Nov', style: style);
-      break;
-    case 11:
-      text = const Text('Dez', style: style);
-      break;
-    default:
-      text = const Text('', style: style);
-}
-
-  return SideTitleWidget(axisSide: meta.axisSide, child: text);
-
 }
 
 double calculateTotalExpenses(List<double> monthExpenses){
--- expense_tracker/lib/pages/home.dart
+++ expense_tracker/lib/pages/home.dart
@@ -1,10 +1,11 @@
 import 'package:expense_tracker/models/enums.dart';
 import 'package:expense_tracker/pages/profile/Profile.dart';
 import 'package:expense_tracker/pages/profile/components/customBottomBar.dart';
+import 'package:expense_tracker/charts/pieChart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:fl_chart/fl_chart.dart';
+
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -18,9 +19,9 @@ class HomePage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           const SizedBox(height: 40.0),
-          SizedBox(
+          const SizedBox(
             height: 300, // Adjust height as needed
-            child: _PieChart(),
+            child: PieChartImpl(),
           ),
           const SizedBox(height: 50.0),
           // Legend
@@ -65,7 +66,7 @@ AppBar buildAppBar(BuildContext context) {
         onTap: () => Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => ProfileScreen(),
+                  builder: (context) => const ProfileScreen(),
                 ),
               ),
         child: Container(
@@ -236,118 +237,3 @@ Icon getCategoryIcon(String category) {
   }
 }
 
-// gráfico de pizza das despesas
-// isso daqui eventualmente tem que estar em outro lugar e procurar arrumar a posicao da legenda
-class _PieChart extends StatelessWidget {
-   const _PieChart();
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        const Text(
-          'Despesas',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            ),
-        ),
-        // Pie Chart
-        PieChart(
-          swapAnimationDuration: const Duration(milliseconds: 750),
-          swapAnimationCurve: Curves.easeInOut,
-          PieChartData(
-            sections: [
-              // saúde
-              PieChartSectionData(
-                value: 20, // esses valores serao mudados pela soma total na parte 2
-                color: const Color.fromARGB(255, 164, 76, 76),
-                radius: 80,
-                borderSide: const BorderSide(color: Colors.black, width: 0.5),
-                title: '20%'
-                ),
-              // lazer
-              PieChartSectionData(
-                value: 20,
-                color: const Color.fromARGB(255, 212, 151, 85),
-                radius: 80,
-                borderSide: const BorderSide(color: Colors.black, width: 0.5),
-                title: '20%'
-                ),
-              // transporte
-              PieChartSectionData(
-                value: 15,
-                color: const Color.fromARGB(255, 19, 108, 181),
-                radius: 80,
-                borderSide: const BorderSide(color: Colors.black, width: 0.5),
-                title: '15%'
-              ),
-              // alimentação
-              PieChartSectionData(
-                value: 20,
-                color: const Color.fromARGB(255, 109, 192, 114),
-                radius: 80,
-                borderSide: const BorderSide(color: Colors.black, width: 0.5),
-                title: '20%'
-                ),
-            ],
-          ),
-        ),
-        ],
-  );
-  }
-}
-
-class Legend extends StatelessWidget {
-  const Legend({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        // Legend items
-        LegendItem(
-          color: Color.fromARGB(255, 164, 76, 76),
-          title: 'Saúde',
-        ),
-        LegendItem(
-          color: Color.fromARGB(255, 212, 151, 85),
-          title: 'Lazer',
-        ),
-        LegendItem(
-          color: Color.fromARGB(255, 19, 108, 181),
-          title: 'Transporte',
-        ),
-        LegendItem(
-          color: Color.fromARGB(255, 109, 192, 114),
-          title: 'Alimentação',
-        ),
-      ],
-    );
-  }
-}
-
-// Selecao das legendas que vao ficar do lado do grafico, essa é a box delas
-class LegendItem extends StatelessWidget {
-  final Color color;
-  final String title;
-
-  const LegendItem({super.key, required this.color, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          width: 30,
-          height: 30,
-          color: color,
-        ),
-        const SizedBox(width: 5),
-        Text(title),
-      ],
-    );
-  }
-}
