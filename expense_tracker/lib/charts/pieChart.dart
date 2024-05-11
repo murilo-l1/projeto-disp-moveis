d212b82d34c5d39e39b91ed6dba52fdf9131e15c
artrosisca <arthur.rosisca@hotmail.com>
2024-05-11 12:09:26 -0300

    sqflite3

--- expense_tracker/lib/charts/pieChart.dart
+++ expense_tracker/lib/charts/pieChart.dart
@@ -3,80 +3,72 @@ import 'package:expense_tracker/services/database_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 
-class PieChartImpl extends StatefulWidget{
+class PieChartImpl extends StatefulWidget {
   const PieChartImpl({super.key});
 
-    @override
-    _PieChartImplState createState() => _PieChartImplState();
+  @override
+  _PieChartImplState createState() => _PieChartImplState();
 }
 
-
 class _PieChartImplState extends State<PieChartImpl> {
-    List<Expense> expenses = [];
+  List<Expense> expenses = [];
 
-    @override
-    void initState() {
-      super.initState();
-      fetchExpenses();
-    }
+  @override
+  void initState() {
+    super.initState();
+    fetchExpenses();
+  }
 
-    Future<void> fetchExpenses() async {
-      List<Expense>? fetchedExpenses = await DatabaseHelper.getExpenses();
+  Future<void> fetchExpenses() async {
+    List<Expense>? fetchedExpenses = await DatabaseHelper.getExpenses();
 
-      if (fetchedExpenses != null && fetchedExpenses.isNotEmpty) {
-        setState(() {
-          expenses = fetchedExpenses;
-        });
-      } else {
-        setState(() {
-          expenses = [];
-        });
-      }
-    }
-
-  
-  List<double> getExpensesByCategory(List<Expense> expenses) {
-  List<double> expensesByCategory = List.filled(4, 0.0);
-
-  for (Expense expense in expenses) {
-    
-    switch (expense.category) {
-      case 'Saúde':
-        expensesByCategory[0] += expense.amount;
-        break;
-      case 'Lazer':
-        expensesByCategory[1] += expense.amount;
-        break;
-      case 'Transporte':
-        expensesByCategory[2] += expense.amount;
-        break;
-      case 'Alimentação':
-        expensesByCategory[3] += expense.amount;
-        break;
-      default:
-        
-        break;
+    if (fetchedExpenses != null && fetchedExpenses.isNotEmpty) {
+      setState(() {
+        expenses = fetchedExpenses;
+      });
+    } else {
+      setState(() {
+        expenses = [];
+      });
     }
   }
 
-  return expensesByCategory;
-}
+  List<double> getExpensesByCategory(List<Expense> expenses) {
+    List<double> expensesByCategory = List.filled(4, 0.0);
+
+    for (Expense expense in expenses) {
+      switch (expense.category) {
+        case 'Saúde':
+          expensesByCategory[0] += expense.amount;
+          break;
+        case 'Lazer':
+          expensesByCategory[1] += expense.amount;
+          break;
+        case 'Transporte':
+          expensesByCategory[2] += expense.amount;
+          break;
+        case 'Alimentação':
+          expensesByCategory[3] += expense.amount;
+          break;
+        default:
+          break;
+      }
+    }
+
+    return expensesByCategory;
+  }
 
   @override
   Widget build(BuildContext context) {
     List<double> expensesByCategory = getExpensesByCategory(expenses);
-    double totalExpenses = expensesByCategory.reduce((a, b) => a + b); 
+    double totalExpenses = expensesByCategory.reduce((a, b) => a + b);
 
     // linha de código pra não ficar um despesas em vão, pode ser melhor só tirar isso no futuro.
-    if(totalExpenses == 0){
+    if (totalExpenses == 0) {
       Alignment.topCenter;
-      return Text(
+      return const Text(
         'Adicione uma despesa para começar!',
-        style: TextStyle(
-          fontSize: 22.0,
-          fontStyle: FontStyle.italic
-
-        ),
+        style: TextStyle(fontSize: 22.0, fontStyle: FontStyle.italic),
       );
     }
 
@@ -88,7 +80,7 @@ class _PieChartImplState extends State<PieChartImpl> {
           style: TextStyle(
             fontSize: 20.0,
             fontWeight: FontWeight.bold,
-            ),
+          ),
         ),
         // Pie Chart
         PieChart(
@@ -102,23 +94,26 @@ class _PieChartImplState extends State<PieChartImpl> {
                 color: const Color.fromARGB(255, 164, 76, 76),
                 radius: 80,
                 borderSide: const BorderSide(color: Colors.black, width: 0.5),
-                title: '${((expensesByCategory[0] / totalExpenses) * 100).toStringAsFixed(1)}%',
-                ),
+                title:
+                    '${((expensesByCategory[0] / totalExpenses) * 100).toStringAsFixed(1)}%',
+              ),
               // lazer
               PieChartSectionData(
                 value: expensesByCategory[1],
                 color: const Color.fromARGB(255, 212, 151, 85),
                 radius: 80,
                 borderSide: const BorderSide(color: Colors.black, width: 0.5),
-                title: '${((expensesByCategory[1] / totalExpenses) * 100).toStringAsFixed(1)}%',
-                ),
+                title:
+                    '${((expensesByCategory[1] / totalExpenses) * 100).toStringAsFixed(1)}%',
+              ),
               // transporte
               PieChartSectionData(
                 value: expensesByCategory[2],
                 color: const Color.fromARGB(255, 19, 108, 181),
                 radius: 80,
                 borderSide: const BorderSide(color: Colors.black, width: 0.5),
-                title: '${((expensesByCategory[2] / totalExpenses) * 100).toStringAsFixed(1)}%',
+                title:
+                    '${((expensesByCategory[2] / totalExpenses) * 100).toStringAsFixed(1)}%',
               ),
               // alimentação
               PieChartSectionData(
@@ -126,13 +121,14 @@ class _PieChartImplState extends State<PieChartImpl> {
                 color: const Color.fromARGB(255, 109, 192, 114),
                 radius: 80,
                 borderSide: const BorderSide(color: Colors.black, width: 0.5),
-                title: '${((expensesByCategory[3] / totalExpenses) * 100).toStringAsFixed(1)}%',
-                ),
+                title:
+                    '${((expensesByCategory[3] / totalExpenses) * 100).toStringAsFixed(1)}%',
+              ),
             ],
           ),
         ),
-        ],
-  );
+      ],
+    );
   }
 }
 
