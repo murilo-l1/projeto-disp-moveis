80dd764ce9893a245b135792418a9c62e53d784b
artrosisca <arthur.rosisca@hotmail.com>
2024-05-11 11:54:36 -0300

    model, service

---
+++ expense_tracker/lib/models/user_model.dart
@@ -0,0 +1,26 @@
+class User {
+  final int? id;
+  final String name;
+  final String email;
+  final String password;
+
+  User(
+      {required this.name,
+      required this.email,
+      required this.password,
+      this.id});
+
+  factory User.fromJson(Map<String, dynamic> json) => User(
+        id: json['id'],
+        name: json['name'],
+        email: json['email'],
+        password: json['password'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'email': email,
+        'password': password,
+      };
+}
---
+++ expense_tracker/lib/services/database_user_helper.dart
@@ -0,0 +1,3 @@
+class DatabaseUserHelper {
+  static cont
+}
