0d96c5604a735a36bf67408e809abfd3d37152c9
murilo-l1 <murilolusvarghi@alunos.utfpr.edu.br>
2024-06-14 14:56:36 -0300

    easyConvert feature fully working

--- expense_tracker/lib/pages/currency-api/services/api_client.dart
+++ expense_tracker/lib/pages/currency-api/services/api_client.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 class ApiClient{
   // colocar apiKey assim que me liberarem!
-  final Uri currencyURL = Uri.https("free.currconv.com", "/api/v7/currencies", {"apiKey": "sample_key"});
+  final Uri currencyURL = Uri.https("free.currconv.com", "/api/v7/currencies", {"apiKey": "b9bc622a2d94d5d33aa6"});
   
   Future<List<String>> getCurrencies() async{
     http.Response response = await http.get(currencyURL);
@@ -12,6 +12,7 @@ class ApiClient{
       var body = jsonDecode(response.body);
       var list = body["results"];
       List <String> currencies = (list.keys).toList();
+      currencies.sort();
       print(currencies);
       return currencies;
   }else{
@@ -22,7 +23,7 @@ class ApiClient{
 Future<double> getConversionRate(String from, String to) async{
   final Uri rateURL = Uri.http('free.currconv.com', '/api/v7/convert', 
   {
-  'apiKey': 'sample_key', 
+  'apiKey': 'b9bc622a2d94d5d33aa6', 
   'q': "${from}_${to}", 
   'compact': 'ultra'});
   http.Response response = await http.get(rateURL);
--- expense_tracker/lib/pages/currency-api/widgets/currency-page.dart
+++ expense_tracker/lib/pages/currency-api/widgets/currency-page.dart
@@ -5,26 +5,23 @@ import 'package:expense_tracker/pages/profile/components/customBottomBar.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/enum/enums.dart';
 
-/*
-class CurrencyPage extends StatefulWidget{
-@override
-_CurrencyPageState createState() => _CurrencyPageState();
+class CurrencyPage extends StatefulWidget {
+  @override
+  _CurrencyPageState createState() => _CurrencyPageState();
 }
 
-class _CurrencyPageState extends State<CurrencyPage>{
-  
+class _CurrencyPageState extends State<CurrencyPage> {
   ApiClient apiClient = ApiClient();
-  //setting the color of the page
-  Color mainColor = Color(0xFF1C1C1C);
-  Color secondaryColor = Color(0xFF292929);
-  
-  // variables to store user selection
+  // Setting the color of the page
+  Color mainColor = Colors.white;
+  Color secondaryColor = const Color(0xFF292929);
   List<String>? currencies;
+
   String? from;
   String? to;
 
-  //Variables to store the conversion rate
-  double? rate;
+  // Variables to store the conversion rate
+  double? rate; // Static conversion rate
   String result = "";
 
   @override
@@ -34,140 +31,11 @@ class _CurrencyPageState extends State<CurrencyPage>{
       List<String> list = await apiClient.getCurrencies();
       setState(() {
         currencies = list;
+        from = list.isNotEmpty ? list[148] : null;
+        to = list.isNotEmpty ? list[19] : null;
       });
-    });
+    })();
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context),
-      backgroundColor: mainColor,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 18.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                width: 200,
-                child: Text(
-                  'EasyConvert',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 36.0,
-                    fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-              SizedBox(height: 25.0),
-              Icon(
-                Icons.currency_exchange,
-                color: Colors.black,
-                size: 70.0,
-              ),
-              Expanded(
-                child: Center(
-                  child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    // campo de insercao do valor em real
-                    TextField(
-                      onSubmitted: (value) async{
-                        rate = await apiClient.getConversionRate(from!, to!);
-                        setState(() {
-                          result = (rate! * double.parse(value)).toStringAsFixed(3);
-                        });
-                      },
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.grey[300],
-                        labelText: "Insira o valor à ser convertido:",
-                        labelStyle: TextStyle(
-                          fontWeight: FontWeight.normal,
-                          fontSize: 18.0,
-                          color: Colors.back)),
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold 
-                    ),
-                    textAlign: TextAlign.center,
-                    keyboardType: TextInputType.number,
-                    ),
-                    SizedBox(height: 20.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        // adicionando agora a caixa que contem as opcoes de conversao
-                        customDropDown(currencies!, to!, (val) {
-                          setState(() {
-                            to = val; 
-                          });
-                         }),
-                         FloatingActionButton(
-                          onPressed: (){
-                            String temp = from!;
-                            setState(() {
-                              from = to;
-                              to = temp;
-                            });
-                            },
-                          child: Icon(Icons.swap_horiz),
-                          elevation: 0.0,
-                          backgroundColor: secondaryColor,
-                         ),
-                      ],
-                    ),
-                    SizedBox(height: 50.0),
-                    Container(
-                      padding: EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                      child: Column(
-                        children: [
-                          Text("Resultado", style: TextStyle(color: Colors.black, fontSize: 24.0, fontWeight: FontWeight.bold )),
-                          Text(result, style: TextStyle(color: secondaryColor, fontSize: 36.0, fontWeight: FontWeight.bold),),
-                        ],
-                        )
-                  )
-                  ],
-                ),
-              ))
-            ],
-          )
-        ),
-      )
-      bottomNavigationBar: const customBottomBarNav(selectedMenu: MenuState.currency),
-    );
-}
-}*/
-
-// ESSA É A IMPLEMENTAÇÃO ESTÁTICA, ASSIM QUE A CHAVE DE API FOR REGULADA ISSO VAI MUDAR PRO C´DIGO ACIMA
-
-class CurrencyPage extends StatefulWidget {
-  @override
-  _CurrencyPageState createState() => _CurrencyPageState();
-}
-
-class _CurrencyPageState extends State<CurrencyPage> {
-  // ApiClient apiClient = ApiClient();
-  // Setting the color of the page
-  Color mainColor = Colors.white;
-  Color secondaryColor = const Color(0xFF292929);
-
-  // Variables to store user selection
-  List<String> currencies = ["USD", "EUR", "BRL"];
-  String from = "USD";
-  String to = "BRL";
-
-  // Variables to store the conversion rate
-  double rate = 5.25; // Static conversion rate
-  String result = "";
 
   @override
   Widget build(BuildContext context) {
@@ -195,10 +63,10 @@ class _CurrencyPageState extends State<CurrencyPage> {
               ),
               SizedBox(height: 25.0),
               Icon(
-                  Icons.currency_exchange,
-                  color: Colors.black,
-                  size: 70.0,
-                ),
+                Icons.currency_exchange,
+                color: Colors.black,
+                size: 70.0,
+              ),
               Expanded(
                 child: Center(
                   child: Column(
@@ -206,53 +74,70 @@ class _CurrencyPageState extends State<CurrencyPage> {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       // Input field for amount
-                      TextField(
-                        onSubmitted: (value) async {
-                          // rate = await apiClient.getConversionRate(from!, to!);
-                          setState(() {
-                            result = (rate * double.parse(value)).toStringAsFixed(3);
-                          });
-                        },
-                        decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.grey[300], // Caixa de texto preenchida com cinza claro
-                          labelText: "Insira o valor a ser convertido:",
-                          labelStyle: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 22.0,
-                            color: Colors.black, // Texto de dica alterado para azul
+                      Flexible(
+                        child: TextField(
+                          onSubmitted: (value) async {
+                            if (from != null && to != null) {
+                              rate = await apiClient.getConversionRate(from!, to!);
+                              setState(() {
+                                result = (rate! * double.parse(value)).toStringAsFixed(3);
+                              });
+                            } else {
+                              throw Exception("From or to are not set!");
+                            }
+                          },
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey[300],
+                            labelText: "Insira o valor a ser convertido:",
+                            labelStyle: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 22.0,
+                              color: Colors.black,
+                            ),
                           ),
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                          keyboardType: TextInputType.number,
                         ),
-                        style: TextStyle(
-                          color: Colors.black, // Texto digitado alterado para azul
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        textAlign: TextAlign.center,
-                        keyboardType: TextInputType.number,
                       ),
                       const SizedBox(height: 20.0),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          // Dropdown for currency selection
-                          customDropDown(currencies, to, (val) {
-                            setState(() {
-                              to = val!;
-                            });
-                          }),
-                          FloatingActionButton(
-                            onPressed: () {
-                              String temp = from;
+                          // Dropdown for selecting 'from' currency
+                          if (currencies != null)
+                            customDropDown(currencies!, from ?? "", (val) {
                               setState(() {
-                                from = to;
-                                to = temp;
+                                from = val;
                               });
+                            }),
+                          FloatingActionButton(
+                            foregroundColor: Colors.grey[200],
+                            onPressed: () {
+                              if (from != null && to != null) {
+                                String temp = from!;
+                                setState(() {
+                                  from = to;
+                                  to = temp;
+                                });
+                              }
                             },
                             child: const Icon(Icons.swap_horiz),
                             elevation: 0.0,
                             backgroundColor: secondaryColor,
                           ),
+                          // Dropdown for selecting 'to' currency
+                          if (currencies != null)
+                            customDropDown(currencies!, to ?? "", (val) {
+                              setState(() {
+                                to = val;
+                              });
+                            }),
                         ],
                       ),
                       const SizedBox(height: 50.0),
